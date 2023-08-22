71ae0fad22a58169e45b53c64d70caa0e072e4ac
Miguel Morera <info@miguelmorera.com>
2023-08-22 13:43:30 +0200

    I converted the project from PHP to a JavaScript approach. Removed Modernizr, jQuery and Picturefill from the project so there is no need to be added in the project. Some CSS adjustments.

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,15 @@ var del         = require('del'),
     stylus      = require('gulp-stylus'),
     rename      = require("gulp-rename"),
     concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
     notify      = require('gulp-notify'),
     cleanCSS    = require('gulp-clean-css'),
     runSequence = require('gulp4-run-sequence');
 
 // The Init task it's for copy the javascript dependencies to the src folder.
 gulp.task('init', function() {
-    gulp.src('node_modules/picturefill/dist/picturefill.min.js')
-        .pipe(gulp.dest('src/assets/js'))
-    gulp.src('node_modules/jquery/dist/jquery.js')
-        .pipe(gulp.dest('src/assets/js/compile/vendor'))
     gulp.src('node_modules/normalize.css/normalize.css')
         .pipe(gulp.dest('src/assets/css/compile/vendor'))
-        .pipe(notify('The initialization of the project has been succesful! (PictureFill/jQuery/Normalize) copied to its folders.'))
+        .pipe(notify('The initialization of the project has been succesful!'))
 });
 
 // Compile Stylus CSS
@@ -34,20 +29,8 @@ gulp.task('style', function (done) {
     done();
 });
 
-// Generate Javascript
-gulp.task('js', function(done){
-    return gulp.src(['src/assets/js/compile/vendor/jquery.js','src/assets/js/compile/vendor/*.js','src/assets/js/compile/*.js'])
-        .pipe(concat('javascript.js'))
-        .pipe(gulp.dest('src/assets/js'))
-        .pipe(notify('JS Compiled!'))
-        .pipe(uglify())
-        .pipe(gulp.dest('src/assets/js'));
-    done();
-});
-
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('src/assets/js/compile/*.js', gulp.series('js'));
     gulp.watch('src/assets/css/compile/styl/*.styl', gulp.series('style'));
 });
 
@@ -70,8 +53,7 @@ gulp.task('copy', function(done) {
 
 gulp.task('build', function(callback) {
     runSequence(
-        'style', 
-        'js', 
+        'style',
         'copy', 
         callback
     );
